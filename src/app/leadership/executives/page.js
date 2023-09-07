7206479c48f2540c4a425a28b6b804fd329ea6fc
Shawn <s.papineau92@gmail.com>
2023-09-07 17:32:22 -0400

    fixed state and page error during build

--- src/app/leadership/executives/page.js
+++ src/app/leadership/executives/page.js
@@ -1,44 +1,12 @@
 'use client'
 import React from 'react'
-import LoginModal from './components/LoginModal'
-import { useState } from 'react'
-import { executives } from '@/executives'
+import PageData from './components/PageData'
 
 function page() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
-  const [user, setUser] = useState('')
-
-  const handleLogin = (login) => {
-    const userInfo = executives.find(
-      (executive) => executive.name == login.username
-    )
-    console.log(userInfo)
-    console.log(login)
-
-    if (userInfo.password === login.password) {
-      setIsLoggedIn(true)
-      setUser(userInfo.name)
-    } else {
-      alert('Incorrect Username or Password. Please Try Again')
-    }
-  }
-
   return (
-    <main>
-      {isLoggedIn ? (
-        <>
-          <h1>Hello, {user}</h1>
-          <p>Nothing here to see yet, but there will be! Stay Tuned!</p>
-        </>
-      ) : (
-        <>
-          <h1 className='text-4xl font-semibold'>
-            Welcome to the Executive Level
-          </h1>
-          <LoginModal handleLogin={handleLogin} />{' '}
-        </>
-      )}
-    </main>
+    <>
+      <PageData />
+    </>
   )
 }
 
